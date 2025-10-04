ef1586bc0f37c2a161ffd7bbdd78a969f81eb658
Adrian Sanchez <adrian.s.duque@gmail.com>
2025-10-04 15:41:59 -0400

    finish project

--- src/gifs/GiftsExpertApp.tsx
+++ src/gifs/GiftsExpertApp.tsx
@@ -1,33 +1,12 @@
-import { useState } from 'react';
 import { CustomHeader } from '../components';
 import { GifsSearch } from './components/GifsSearch';
 import { PreviosResearches } from './components/PreviousResearch';
 import GifsList from './components/GifsList';
-import { getGifBySearch } from '../action/get-gif-by-search.action';
-import type { Gif } from '../interfaces';
+import { useGifs } from './hook/useGifs';
 
 export default function App() {
 
-  const [previousGifs, setPreviousGifs] = useState<string[]>([])
-  const [gifs, setGifs] = useState<Gif[]>([])
-
-  const onLabelClicked = (label: string) => {
-    console.log(label)
-  }
-
-  const handleSearch = async (search: string) => {
-    //* Si el parámetro search esta vacio con espacios para la ejecución
-    if( !search.trim().length ) return
-    //* Transformamos el parámetro de search
-    const searchFormatter = search.trim().toLowerCase()
-    //* Si el search existe en previousGifs para la ejecución de la función
-    if(previousGifs.includes(search)) return
-    //* Agregamos el search a previousGifs y limitamos el número de elementos a 8
-    setPreviousGifs( [ searchFormatter, ...previousGifs.splice(0, 8) ] )
-    const actionRepsonse = await getGifBySearch(search)
-    
-      setGifs(actionRepsonse)
-  }
+  const { gifs, handleSearch, previousGifs, onLabelClicked } = useGifs()
 
   return (
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import GifsExpertApp from './gifs/GiftsExpertApp.tsx'
+//import { MyCounterApp } from './MyCounterApp/MyCounterApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* <MyCounterApp /> */}
     <GifsExpertApp />
   </StrictMode>,
 )
